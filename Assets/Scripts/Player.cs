9ca6388bafa70168bbf5f82bfdf7a3698447f0a4
Tim <herew26@gmail.com>
2022-08-26 20:06:49 +0300

    refactor: move jump permission logic to other component

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.Mathf;
 
+[RequireComponent(typeof(JumpPermission))]
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
     private Vector2 _moveInput;
     private RaycastHit2D _raycastHit;
-
-    private float _distanceFromGround;
-    private float _allowedDistanceToJump = 1.3f;
-
+    private JumpPermission _jumpPermission;
+    
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _jumpForce;
-
+    
     private void Start()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _jumpPermission = GetComponent<JumpPermission>();
     }
-
+    
     private void Update()
     {
-        _raycastHit = Physics2D.Raycast(_rigidBody.position, Vector2.down);
-        if (_raycastHit.collider != null)
-        {
-            _distanceFromGround = _raycastHit.distance;
-        }
         TryMove();
     }
 
@@ -40,10 +34,10 @@ public class Player : MonoBehaviour
         var input = value.Get<Vector2>();
         _moveInput = input;
     }
-
+    
     private void OnJump(InputValue value)
     {
-        if (_distanceFromGround > _allowedDistanceToJump) return;
+        if (_jumpPermission.AllowedToJump is false) return;
 
         var velocity = new Vector2(0, _jumpForce);
         _rigidBody.velocity = velocity;
