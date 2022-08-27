64fb4d7d4c37f3064cda6a4581fc329e556dc94d
Tim <herew26@gmail.com>
2022-08-27 14:25:00 +0300

    feat: change distance from predefined parameter to calculated

--- Assets/Scripts/JumpPermission.cs
+++ Assets/Scripts/JumpPermission.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Transform))]
 public class JumpPermission : MonoBehaviour
 {
-    [SerializeField] private float _allowedDistanceFromGroundToJump;
-    
+    [SerializeField] private BoxCollider2D _collider;
+
+    private const float AllowedDistanceFromGroundToJump = 0.1f;
     private Rigidbody2D _rigidbody;
 
     private void Start()
@@ -15,16 +14,19 @@ public class JumpPermission : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    public bool AllowedToJump => TryGetDistanceFromGround(_rigidbody, out float distanceFromGround)
-                                 && distanceFromGround <= _allowedDistanceFromGroundToJump;
+    public bool AllowedToJump => TryGetDistanceFromGround(out float distanceFromGround)
+                                 && distanceFromGround <= AllowedDistanceFromGroundToJump;
     
-    private static bool TryGetDistanceFromGround(Rigidbody2D rigidBody, out float distance)
+    private bool TryGetDistanceFromGround(out float distance)
     {
         distance = 0;
-        var raycastHit = Physics2D.Raycast(rigidBody.position, Vector2.down);
         
+        var position = _rigidbody.position;
+        var vector = new Vector2(position.x, position.y - transform.localScale.y * _collider.size.y / 2);
+        var raycastHit = Physics2D.Raycast(vector, Vector2.down);
+
         if (raycastHit.collider == null) return false;
-        
+
         distance = raycastHit.distance;
         return true;
 
