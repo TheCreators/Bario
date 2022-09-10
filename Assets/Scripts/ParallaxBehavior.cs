5d8e6694320ca176226a149545659ceb0a8c62a0
Tim <herew26@gmail.com>
2022-09-10 15:20:35 +0300

    refactor: everything

--- Assets/Scripts/ParallaxBehavior.cs
+++ Assets/Scripts/ParallaxBehavior.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 public class ParallaxBehavior : MonoBehaviour
 {
-    [SerializeField] private Transform followingTarget;
-    [SerializeField, Range(0f, 1f)] private float parallaxStrength = 0.1f;
-    [SerializeField] private bool disableVerticalParallax;
+    [SerializeField] private Transform _followingTarget;
+    [SerializeField, Range(0f, 1f)] private float _parallaxStrength = 0.1f;
+    [SerializeField] private bool _disableVerticalParallax;
     private Vector3 _targetPreviousPosition;
-    void Start()
+
+    private void Start()
     {
-        if (!followingTarget)
-            followingTarget = UnityEngine.Camera.main.transform;
-        _targetPreviousPosition = followingTarget.position;
+        if (_followingTarget == null)
+        {
+            Debug.Assert(UnityEngine.Camera.main != null, "UnityEngine.Camera.main != null");
+            _followingTarget = UnityEngine.Camera.main.transform;
+        }
+
+        _targetPreviousPosition = _followingTarget.position;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        var delta = followingTarget.position - _targetPreviousPosition;
-        if (disableVerticalParallax is true) delta.y = 0;
-        _targetPreviousPosition = followingTarget.position;
-        transform.position += delta * parallaxStrength;
+        MoveObject();
+    }
+
+    private void MoveObject()
+    {
+        var delta = _followingTarget.position - _targetPreviousPosition;
+        
+        if (_disableVerticalParallax is true) delta.y = 0;
+        
+        _targetPreviousPosition = _followingTarget.position;
+        transform.position += delta * _parallaxStrength;
     }
 }
